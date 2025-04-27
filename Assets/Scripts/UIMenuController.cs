9a8f24391dbc73c77a32d1a5d1872a4f7f72d295
ploy <ployw@unr.edu>
2025-04-26 18:11:26 -0700

    Add controls panel to UI menu

--- Assets/Scripts/UIMenuController.cs
+++ Assets/Scripts/UIMenuController.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class UIMenuController : MonoBehaviour
 {
   [SerializeField] private GameObject menuPanel;
   [SerializeField] private GameObject changeEnvironmentPanel;
   [SerializeField] private GameObject viewOptionsPanel;
+  [SerializeField] private GameObject viewControlsPanel;
 
   [SerializeField] private Button startButton;
   [SerializeField] private Button changeButton;
   [SerializeField] private Button exitButton;
   [SerializeField] private Button closeButton;
   [SerializeField] private Button viewOptionsButton;
-  [SerializeField] private Button returnButton;
+  [SerializeField] private Button viewControlsButton;
+  [SerializeField] private  List<Button> returnButtons;
 
   void Start()
   {
@@ -22,7 +25,12 @@ public class UIMenuController : MonoBehaviour
     viewOptionsButton.onClick.AddListener(() => OpenPanel(menuPanel));
     changeButton.onClick.AddListener(() => OpenPanel(changeEnvironmentPanel));
     closeButton.onClick.AddListener(() => OpenPanel(viewOptionsPanel));
-    returnButton.onClick.AddListener(() => OpenPanel(menuPanel));
+    viewControlsButton.onClick.AddListener(() => OpenPanel(viewControlsPanel));
+
+    foreach(Button button in returnButtons)
+    {
+      button.onClick.AddListener(() => OpenPanel(menuPanel));
+    }
     
     startButton.onClick.AddListener(StartGame);
     exitButton.onClick.AddListener(ExitGame);
@@ -46,6 +54,7 @@ public class UIMenuController : MonoBehaviour
     menuPanel.SetActive(false);
     changeEnvironmentPanel.SetActive(false);
     viewOptionsPanel.SetActive(false);
+    viewControlsPanel.SetActive(false);
     
     panelToOpen.SetActive(true);
   }
