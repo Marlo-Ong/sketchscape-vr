bd6d4f207282f41cb65e5abe68504512425aa24d
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-05-06 14:17:39 -0700

    Fix draw haptics

--- Assets/Scripts/DrawHaptics.cs
+++ Assets/Scripts/DrawHaptics.cs
@@ -1,15 +1,16 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Feedback;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
 [RequireComponent(typeof(HapticImpulsePlayer))]
 public class DrawHaptics : MonoBehaviour
 {
-    [SerializeField] private HapticImpulseData properties;
     [Tooltip("Which brush (controller) to send haptic feedback to")]
     [SerializeField] private TexturePainter.Brush brush;
 
     private HapticImpulsePlayer haptics;
+    private Vector3 previousPosition;
+    private Coroutine paintHapticLoop;
 
     private void Start()
     {
@@ -19,23 +20,46 @@ public class DrawHaptics : MonoBehaviour
     private void OnEnable()
     {
         TexturePainter.OnStartPainting += this.PlayHaptics;
+        TexturePainter.OnStopPainting += this.StopHaptics;
     }
 
     private void OnDisable()
     {
         TexturePainter.OnStartPainting -= this.PlayHaptics;
+        TexturePainter.OnStopPainting -= this.StopHaptics;
     }
 
-    private void PlayHaptics(TexturePainter.Brush brush)
+    private void StopHaptics(TexturePainter.Brush inputState)
     {
-        if (brush != this.brush)
+        // Only stop when this brush flag is gone AND we actually have a loop running.
+        if (inputState.HasFlag(this.brush) || this.paintHapticLoop == null)
             return;
 
-        this.PlayHaptics();
+        StopCoroutine(paintHapticLoop);
+        this.paintHapticLoop = null;
     }
 
-    private void PlayHaptics()
+    private void PlayHaptics(TexturePainter.Brush inputState)
     {
-        this.haptics.SendHapticImpulse(this.properties.amplitude, this.properties.duration);
+        // If this brush just turned on, and we're not already looping, start once.
+        if (!inputState.HasFlag(this.brush) || this.paintHapticLoop != null)
+            return;
+
+        // Reset the previous position so the very first frame isn't a giant spike.
+        this.previousPosition = this.transform.position;
+        this.paintHapticLoop = StartCoroutine(ContinuePaintHaptics(this.transform));
+    }
+
+    private IEnumerator ContinuePaintHaptics(Transform tr)
+    {
+        while (true)
+        {
+            var pos = tr.position;
+            var velocity = (pos - this.previousPosition) / Time.deltaTime;
+            float amplitude = Mathf.Clamp(velocity.sqrMagnitude, 0, 1);
+            this.haptics.SendHapticImpulse(amplitude, .2f);
+            this.previousPosition = pos;
+            yield return null;
+        }
     }
 }
